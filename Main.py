46b6471e98e902a2cd93a18cc22de4837c0e45d4
Ghyovani Vielot <71789236+ghyothehero@users.noreply.github.com>
2021-07-14 00:19:55 +0200

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,115 @@
+#what we use to "create new board object"
+import random
+import re
+class Board:
+    def __init__(self, dim_size, num_bombs):
+        #keeps track of parameters
+        self.dim_size = dim_size
+        self.num_bombs = num_bombs
+
+        #helper Function
+        self.board = self.make_new_board() #plantBombs
+        self.assign_values_to_board()
+
+
+        self.dug=set() #if we dig at 0,0 then self.dug = {(0,0)}
+
+
+        pass
+
+def make_new_board(self):
+    #construct a new board based on the dim size and num bombs
+    board = [[None for _ in range(self.dim_size)]for _ in range(self.dim_size)]
+
+    bombs_planted = 0 
+    while bombs_planted < self.num_bombs:
+        loc = random.randint(0, self.dim_size**2 -1)
+        row = loc // self.dim_size
+        col = loc % self.dim_size
+
+        if board[row][col] == '*':
+            #means bobm is actually planted 
+            continue
+
+        board[row][col] ='*'
+        bombs_planted += 1
+
+        return board
+
+def assign_values_to_board(self):
+    for r in range(self.dim_size):
+        for c in range(self.dim_size):
+            if self.board[r][c] == '*':
+                continue
+            self.board[r][c] = self.get_num_neighboring_bombs(r,c)
+
+def get_num_neighboring_bombs(self,row,col):
+    num_neighboring_bombs = 0 
+    for r in range(max(0, row-1, min(self.dim_size-1,(row+1)+1))):
+        for c in range(max(0, col-1, min(self.dim_size -1,col+1)+1)):
+            if r == row and c == col:
+
+                continue
+            if self.board[r][c] == '*':
+                num_neighboring_bombs += 1
+
+    return num_neighboring_bombs
+
+def dig(self, row, col):
+    if self.dug.add((row, col)) == '*':
+        return False
+    elif self.board[row][col] > 0:
+        return True
+   
+    for r in range(max(0, row-1, min(self.dim_size-1,(row+1)+1))):
+        for c in range(max(0, col-1, min(self.dim_size -1,col+1)+1)):
+            if (r,c) in self.dug:
+                continue
+            self.dig(r,c)
+
+    return True
+
+def __str__(self):
+    #magic function where if you call print on this object 
+    #itll print out this function returns 
+
+    visible_board = [[None for _ in range(self.dim_size)] for _ in range(self.dim_size)]
+    for  row in range (self.dim_size):
+        for  col in range (self.dim_size):
+            if (row,col) in self.dug:
+                visible_board[row][col] = str(self.board[row][col])
+        else:
+                visible_board[row][col] = ''
+
+#play the game
+def play(dim_size=10,num_bombs = 10):
+
+    board = Board(dim_size,num_bombs)
+
+    while len(board.dug) < board.dim_size ** 2 - num_bombs:
+        print(board)
+        user_input = re.split(',(\\s)*',input("Where would you like to dig? input as row,col: "))
+        row, col = int(user_input[0]), int(user_input[-1])
+        if row < 0 or row >= board.dim_size or col < 0 or col>= dim_size:
+            print("Invalid location. Try again")
+        continue
+    
+    #if its valid we dig
+    safe = board.dig(row, col)
+    if not safe:
+            # dug a bomb!
+        break #(game over)
+
+# 2 ways to end loop 
+    if safe:
+        print("CONGRATULATIONS!! YOU AR VICTORIOUS")
+    else   :
+        print("SORRY GAME OVER!")
+
+        board.dug = [(r,c) for r in range(board.dim_size) for c in range(board.dim_size)]
+        
+        print(board)
+
+    if __name__ == '__main__':
+        play()
+
